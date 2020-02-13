893bf753556941b3b241373bd832b4d0ad1ee8c3
leena <leena@ssfs.org>
2020-02-13 14:15:57 -0500

    2/13/20

---
+++ src/ReviewDriver.java
@@ -0,0 +1,7 @@
+public class ReviewDriver {
+    public static void main(String[]args){
+        System.out.println(Review.sentimentVal("monster"));
+        System.out.println(Review.sentimentVal("power"));
+        System.out.println(Review.sentimentVal("universe"));
+    }
+}
